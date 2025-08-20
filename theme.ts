d6295a42dde3a93606543096f31c67ff559c837d
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-08-20 02:19:48 -0600

    style: add colors to theme.ts

--- theme.ts
+++ theme.ts
@@ -1,5 +1,17 @@
 export const theme = {
-    colorCerulean: "#1a759f",
-    colorWhite: "#fff",
-    colorBlack: "#000",
+  colorCerulean: "#5DADE2",
+  colorWhite: "#fff",
+  colorOffWhite: "#fafafa",
+
+  colorBlack: "#000",
+  colorLightBlue: "#85C1E9",
+  colorSoftPurple: "#BB8FCE",
+  colorCoral: "#F1948A",
+  colorSalmonRed: "#CD6155",
+  colorLightGrey: "#EEEEEE",
+  colorGrey: "#999",
+  colorDarkGrey: "#666",
+  colorSage: "#A2AF9B",
+  colorCream: "#DCCFC0",
+  colorPeach: "#eba39c",
 };
